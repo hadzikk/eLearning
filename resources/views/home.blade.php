be8543e6acbbf17fe94341607201103b63329aa8
hadzikk <hadzikmochammad@gmail.com>
2024-06-22 13:32:29 +0700

    menambahkan halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,53 @@
 <x-layout>
-    <p class="text-2xl text-white">home</p>
+    {{-- Navigation --}}
+    <x-navbar></x-navbar>
+
+    {{-- Container --}}
+    <div class="min-w-full min-h-screen">
+        <div class="min-w-full flex gap-2 py-4 px-5 flex-wrap">
+            <div class="w-80  h-60 bg-neutral-950 shadow-2xl shadow-black rounded">
+                <div class="w-full h-4/6 overflow-hidden">
+                    <img class="w-full rounded" src="{{ asset('images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg') }}" alt="">
+                </div>
+                <div class="w-full h-1/5 py- px-5">
+                    <p class="font-nunito text-orange-600">JavaScript</p>
+                    <p class="text-white capitalize font-nunito font-bold">mempelajari bahasa pemrograman javascript</p>
+                </div>
+            </div>
+
+            <div class="w-80  h-60 bg-neutral-950 shadow-2xl shadow-black rounded">
+                <div class="w-full h-4/6 overflow-hidden">
+                    <img class="w-full rounded" src="{{ asset('images/Google Collab.png') }}" alt="">
+                </div>
+                <div class="w-full h-1/5 py- px-5">
+                    <p class="font-nunito text-orange-600">Python</p>
+                    <p class="text-white capitalize font-nunito font-bold">mempelajari data analyst menggunakan google collab</p>
+                </div>
+            </div>
+
+            <div class="w-80  h-60 bg-neutral-950 shadow-2xl shadow-black rounded">
+                <div class="w-full h-4/6 overflow-hidden">
+                    <img class="w-full rounded" src="{{ asset('images/GO logo.png') }}" alt="">
+                </div>
+                <div class="w-full h-1/5 py- px-5">
+                    <p class="font-nunito text-orange-600">GO</p>
+                    <p class="text-white capitalize font-nunito font-bold">membangun back end menggunakan go</p>
+                </div>
+            </div>
+            
+            <div class="w-80  h-60 bg-neutral-950 shadow-2xl shadow-black rounded">
+                <div class="w-full h-4/6 overflow-hidden">
+                    <img class="w-full rounded" src="{{ asset('images/markus-spiske-MgtHZ4zlC1U-unsplash.jpg') }}" alt="">
+                </div>
+                <div class="w-full h-1/5 py- px-5">
+                    <p class="font-nunito text-orange-600">JavaScript</p>
+                    <p class="text-white capitalize font-nunito font-bold">mempelajari bahasa pemrograman javascript</p>
+                </div>
+            </div>
+        </div>
+        <div class="min-w-full flex gap-2 py-4 px-5 flex-wrap">
+            <div class="w-80  h-60 bg-white shadow rounded"></div>
+        </div>
+    </div>
+    <x-footer></x-footer>
 </x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,17 @@
 <x-layout>
     {{-- Navigation --}}
-    <x-navbar></x-navbar>
+    <nav class="min-w-full py-7 px-5 shadow flex justify-between items-center">
+        <div class="items-center flex">
+            <p class="font-raleway text-2xl font-bold mr-16 text-white">eLearning</p>
+        </div>
+        <div>
+            <a href="" class=" px-5 text-white">login</a>
+            <a href="" class=" px-5 text-white">sign up</a>
+        </div>
+    </nav>
 
     {{-- Container --}}
-    <div class="min-w-full h-screen flex gap-4 py-3 px-5">
+    <div class="min-w-full h-screen flex gap-4 py-3 px-5 relative overflow-x-hidden overflow-y-hidden">
         <x-login></x-login>
         {{-- Content --}}
         <div class="w-2/4 min-h-full py-5 px-5 bg-yellow-300 shadow rounded">
@@ -12,12 +20,13 @@
         </div>
         {{-- Content --}}
         <div class="w-2/4 min-h-full bg-white shadow rounded overflow-hidden relative">
-            {{-- <img src="{{ asset('images/annie-spratt-vGgn0xLdy8s-unsplash.jpg') }}" alt="Online Learning Illustration" class="w-full h-full rounded object-cover absolute z-0"> --}}
+            <img src="{{ asset('images/annie-spratt-vGgn0xLdy8s-unsplash.jpg') }}" alt="Online Learning Illustration" class="w-full h-full rounded object-cover absolute z-0">
             <div class="absolute z-10 py-5 px-5">
                 <p class="text-2xl font-bold w-96 text-white">belajar dimana saja</p>
             </div>
         </div>
     </div>
-
     <x-footer></x-footer>
+
+    <script src="{{ asset('js/script.js') }}"></script>
 </x-layout>
